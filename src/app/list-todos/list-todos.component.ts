27bc36d88e3e0128936c177b2a686588eaad9562
karanbhalla <kbhalla22@gmail.com>
2019-05-19 12:46:25 -0400

    added bootstrap classes

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+export class todo{
+  constructor(
+    public id: number,
+    public description: string,
+    public done:boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
 
 @Component({
   selector: 'app-list-todos',
@@ -7,9 +17,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos=[
-    {id:1, description:'Learn to dance'},
-    {id:2, description:'Become an expert at Angular'},
-    {id:3, description:'visit London'}
+    new todo(1,'Learn to Dance',false, new Date()),
+    new todo(2,'Become an expert at Angular',false, new Date()),
+    new todo(3,'visit London',false, new Date())
+    // {id:1, description:'Learn to dance'},
+    // {id:2, description:'Become an expert at Angular'},
+    // {id:3, description:'visit London'}
   ]
 // todo={
 //   id: 1,
