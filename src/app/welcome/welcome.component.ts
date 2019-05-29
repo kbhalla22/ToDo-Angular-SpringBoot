972cde404cfd8c9d29c437b6e2e606fa1b3b6661
karanbhalla <kbhalla22@gmail.com>
2019-05-29 11:30:28 -0400

    created a new service to fetch welcome data

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -20,5 +20,7 @@ export class WelcomeComponent implements OnInit {
     //console.log(this.route.snapshot.params['name'])
    this.name =this.route.snapshot.params['name']
   }
-
+getWelcomeMessage(){
+  console.log("get welcome message");
+}
 }
