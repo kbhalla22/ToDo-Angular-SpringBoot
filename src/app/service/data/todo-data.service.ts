3c58d30a25df45b484037519738ad95f61274007
karanbhalla <kbhalla22@gmail.com>
2019-06-05 15:50:47 -0400

    added the routing after save button

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -15,9 +15,12 @@ export class TodoDataService {
    // console.log("execute hello world bean service")
   }
   deleteTodo(username,id){
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`)
+    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
   }
   retrieveTodo(username,id){
-    return this.http.get<todo>(`http://localhost:8080/users/${username}/todos/${id}`)
+    return this.http.get<todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+  updateTodo(username,id,todo){
+    return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`,todo);
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -14,7 +14,8 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoDataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -22,6 +23,13 @@ export class TodoComponent implements OnInit {
     this.todo=new todo(1,'',false,new Date());
     this.todoService.retrieveTodo('karrybee',this.id).subscribe(
 data => this.todo =data
+    )
+  }
+  saveTodo(){
+    this.todoService.updateTodo('karrybee',this.id,this.todo).subscribe(
+      data=>{
+        console.log(data)
+      this.router.navigate(['todos'])}
     )
   }
 
