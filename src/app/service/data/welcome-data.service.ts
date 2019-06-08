4896c404fdfc2dd74fd1e8b6f90e5de8a57ac0a4
karanbhalla <kbhalla22@gmail.com>
2019-06-08 11:31:04 -0400

    Added code for integrating spring security

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 
 export class HelloWorldBean{
@@ -22,6 +22,19 @@ export class WelcomeDataService {
   }
  // http://localhost:8080/hello-world/path-variable/karrybee
   executeHelloWorldServiceWithPathVariable(name){
-return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+    let basicAuthHeaderString=this.createBasicAuthenticationHttpHeader();
+    let headers=new HttpHeaders(
+      {
+        Authorization:basicAuthHeaderString
+      }
+    )
+return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`,
+{headers});
+  }
+  createBasicAuthenticationHttpHeader(){
+    let username='karrybee'
+    let password='dummy'
+    let basicAuthHeaderString ='Basic' +window.btoa(username + ':' +password);
+    return basicAuthHeaderString;
   }
 }
