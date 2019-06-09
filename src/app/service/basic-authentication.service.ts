e286762bbecbdf0487eeb5052838bfd04172fc92
karanbhalla <kbhalla22@gmail.com>
2019-06-08 20:12:14 -0400

    refactored the code to use constants

--- src/app/service/basic-authentication.service.ts
+++ src/app/service/basic-authentication.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { API_URL } from '../app.constants';
 
+export const TOKEN = 'token'
+export const AUTHENTICATED_USER = 'authenticateUser';
 @Injectable({
   providedIn: 'root'
 })
@@ -18,34 +21,34 @@ export class BasicAuthenticationService {
         Authorization:basicAuthHeaderString
       }
     )
-return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
+return this.http.get<AuthenticationBean>(`${API_URL}/basicauth`,
 {headers}).pipe(
   map(
     data=>{
-      sessionStorage.setItem('authenticateUser',username);
-      sessionStorage.setItem('token',basicAuthHeaderString);
+      sessionStorage.setItem( AUTHENTICATED_USER,username);
+      sessionStorage.setItem(TOKEN,basicAuthHeaderString);
       return data;
     }
   )
 );
   }
   getAuthenticatedUser(){
-    return  sessionStorage.getItem('authenticateUser')
+    return  sessionStorage.getItem( AUTHENTICATED_USER)
    
       }
       getAuthenticatedToken(){
         if(this.getAuthenticatedUser())
-        return  sessionStorage.getItem('token')
+        return  sessionStorage.getItem(TOKEN)
        
           }
   
   isUserLoggedIn(){
-let user=sessionStorage.getItem('authenticateUser')
+let user=sessionStorage.getItem( AUTHENTICATED_USER)
 return!(user===null);
   }
   logout(){
-    sessionStorage.removeItem('authenticateUser')
-    sessionStorage.removeItem('token')
+    sessionStorage.removeItem( AUTHENTICATED_USER)
+    sessionStorage.removeItem(TOKEN)
   }
  
 }
--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { todo } from 'src/app/list-todos/list-todos.component';
+import { API_URL } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +12,19 @@ export class TodoDataService {
     private http:HttpClient
   ) { }
  retrieveAllTodos(username){
-    return this.http.get<todo[]>(`http://localhost:8080/users/${username}/todos`);
+    return this.http.get<todo[]>(`${API_URL}/users/${username}/todos`);
    // console.log("execute hello world bean service")
   }
   deleteTodo(username,id){
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`);
   }
   retrieveTodo(username,id){
-    return this.http.get<todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.get<todo>(`${API_URL}/users/${username}/todos/${id}`);
   }
   updateTodo(username,id,todo){
-    return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`,todo);
+    return this.http.put(`${API_URL}/users/${username}/todos/${id}`,todo);
   }
   createTodo(username,todo){
-    return this.http.post(`http://localhost:8080/users/${username}/todos`,todo);
+    return this.http.post(`${API_URL}/users/${username}/todos`,todo);
   }
 }
